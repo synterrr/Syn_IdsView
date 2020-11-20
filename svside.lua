20dbb952161d8d0061ed75eb3f23c6c82830194b
SynTer <alpacanismo@gmail.com>
2020-11-20 19:33:17 -0300

    Update svside.lua

--- svside.lua
+++ svside.lua
@@ -35,30 +35,33 @@ if vRP == true then
     end)     
 end
 
-if ESX == true then
+if Esx == true then
+ESX = nil
+    local xPlayer = ESX.GetPlayerFromId(source)
+
     RegisterCommand('printids',function(source,args,rawCommand)
-        if IsPlayerAceAllowed(source, "view.ids") then
-            local steamid  = false
-            local license  = false
-            local discord  = false
-            for k,v in pairs(GetPlayerIdentifiers(args[1]))do
-                if string.sub(v, 1, string.len("steam:")) == "steam:" then
-                    steamid = v
-                elseif string.sub(v, 1, string.len("license:")) == "license:" then
-                    license = v
-                elseif string.sub(v, 1, string.len("discord:")) == "discord:" then
-                    discord = v 
-                        if discord == false then 
-                            discord = "DISCORD ISN'T OPENED"
-                            if steamid == false then 
-                                steamid = 'STEAM NOT OPENED'
-                                if args[1] then 
-                                    TriggerClientEvent('chat:addMessage', {color = { 255, 0, 0},multiline = true,args = {"Me", "Those are the following identifiers of the ID:"..args[1].."\n"..discord.."\n"..steamid.."\n"..license}})
-                                end
-                        end
+            if xPlayer.getGroup() == "admin" then
+                    local steamid  = false
+                    local license  = false
+                    local discord  = false
+                    for k,v in pairs(GetPlayerIdentifiers(args[1]))do
+                            if string.sub(v, 1, string.len("steam:")) == "steam:" then
+                                    steamid = v
+                            elseif string.sub(v, 1, string.len("license:")) == "license:" then
+                                    license = v
+                            elseif string.sub(v, 1, string.len("discord:")) == "discord:" then
+                                    discord = v 
+                                            if discord == false then 
+                                                    discord = "DISCORD ISN'T OPENED"
+                                                    if steamid == false then 
+                                                            steamid = 'STEAM NOT OPENED'
+                                                            if args[1] then 
+                                                                    TriggerClientEvent('chat:addMessage', {color = { 255, 0, 0},multiline = true,args = {"Me", "Those are the following identifiers of the ID:"..args[1].."\n"..discord.."\n"..steamid.."\n"..license}})
+                                                            end
+                                            end
+                                    end
+                            end
                     end
-                end
             end
-        end
     end)
 end
